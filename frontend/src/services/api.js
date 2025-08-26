dacdcfe99b7a23143ef0c52664940d112f6e118c
Sai Khant Min Bhone <saikhantminbhone@gmail.com>
2025-08-26 15:07:48 +0700

    fix file uploader and contract AI checker

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -71,7 +71,6 @@ export const verifyDocument = async (file) => {
     const formData = new FormData();
     formData.append('contract', file);
     const { data } = await api.post('/verify-document', formData);
-    alert(data)
     return data;
 };
 
